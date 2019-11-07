e1ca13687503cd0971fbfafc84aeef5196550749
meji <mejias@raiz.es>
2019-11-07 19:49:36 +0100

    Hovers cabecera, botones y links

--- js/js.js
+++ js/js.js
@@ -391,3 +391,60 @@ function hoverCareers(){
 		card.removeClass('hover');
 	});
 }
+
+/*
+ * Replace all SVG images with inline SVG
+ */
+$('#footer .social img[src$=".svg"]').each(function(){
+    var $img = $(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+    var imgAlt = $img.attr('alt');
+    var imgTitle = $img.attr('title');
+
+    jQuery.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        //Width y height
+        if(!$svg.attr('viewBox')){
+	        $svg.attr('viewBox', ('0 0 '
+	          + $svg.attr('width').match(/[0-9]+\.[0-9]*/) + ' '
+	          + $svg.attr('height').match(/[0-9]+\.[0-9]*/)));
+	    }
+
+	    //Alt
+	    // add replaced image's alt tag to the new SVG
+		if(typeof imgAlt !== 'undefined') {
+		    $svg = $svg.attr('alt', imgAlt);
+		}
+
+	    //Title
+	    // add replaced image's alt tag to the new SVG
+		if(typeof imgTitle !== 'undefined') {
+		    $svg = $svg.attr('title', imgTitle);
+		}
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+});
+
